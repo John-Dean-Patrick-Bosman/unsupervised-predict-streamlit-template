05397a6324ac1908fcfa950dc3566fc29050c2e9
John-Dean-Patrick-Bosman <81234395+John-Dean-Patrick-Bosman@users.noreply.github.com>
2021-07-22 11:49:57 +0200

    Update content_based.py

--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -82,7 +82,7 @@ def content_model(movie_list,top_n=10):
     recommended_movies = []
     data = data_preprocessing(27000)
     # Instantiating and generating the count matrix
-    count_vec = CountVectorizer()
+    count_vec = CountVectorizer(strip_accents = "unicode", stop_words = "english", ngram_range = (1,2) #editted
     count_matrix = count_vec.fit_transform(data['keyWords'])
     indices = pd.Series(data['title'])
     cosine_sim = cosine_similarity(count_matrix, count_matrix)
